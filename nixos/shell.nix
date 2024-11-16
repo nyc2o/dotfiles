2d63413ef88e19a256f692b6fc268fb42f5cef29
nycto <nycto>
2024-11-16 22:01:00 +0800

    build from src—SBCL newer version

--- nixos/shell.nix
+++ nixos/shell.nix
@@ -9,7 +9,12 @@ stdenv.mkDerivation {
 
   buildInputs = [
     emacs
-    sbcl
+    (sbcl.overrideAttrs (old: {
+      src = pkgs.fetchurl {
+        url = "https://sourceforge.net/projects/sbcl/files/sbcl/2.4.10/sbcl-2.4.10-source.tar.bz2";
+        sha256 = "sha256-zus5a2nSkT7uBIQcKva+ylw0LOFGTD/j5FPy3hDF4vg=";
+      };
+    }))
     cl-launch
     which
     rlwrap
@@ -23,5 +28,5 @@ stdenv.mkDerivation {
     libfixposix
   ];
 
-  LD_LIBRARY_PATH = lib.strings.makeLibraryPath [ openssl sqlite];
+  LD_LIBRARY_PATH = lib.strings.makeLibraryPath [ openssl sqlite ];
 }
